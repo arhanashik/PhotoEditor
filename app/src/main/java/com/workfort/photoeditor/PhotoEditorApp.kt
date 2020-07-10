ba616516997b63b772eeedb9f1ee907a8eb8e817
arhan.ashik <ashik.pstu.cse@gmail.com>
2020-07-10 18:53:40 +0900

    read me update

---
+++ app/src/main/java/com/workfort/photoeditor/PhotoEditorApp.kt
@@ -0,0 +1,51 @@
+package com.workfort.photoeditor
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.workfort.base.BaseApp
+import timber.log.Timber
+
+open class PhotoEditorApp : BaseApp() {
+    init {
+        sInstance = this
+    }
+
+    companion object {
+        private lateinit var sInstance: PhotoEditorApp
+
+        fun getApplicationContext(): Context {
+            return sInstance.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+//        SystemClock.sleep(300)
+
+        if (applicationContext != null) {
+            if (BuildConfig.DEBUG) {
+                initiateOnlyInDebugMode()
+            }
+//            initiate(applicationContext)
+        }
+    }
+
+    private fun initiateOnlyInDebugMode() {
+        Timber.plant(object : Timber.DebugTree() {
+            override fun createStackElementTag(element: StackTraceElement): String? {
+                return super.createStackElementTag(element) +
+                        " - Method:${element.methodName} - Line:${element.lineNumber}"
+            }
+        })
+    }
+
+//    private fun initiate(context: Context) {
+//        Prefs.init(context)
+//    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
